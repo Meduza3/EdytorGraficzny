9f0dee8073c043c7ea5d1b1de8ba37d97d8045f2
Meduza3 <jellyfish200325@gmail.com>
2023-05-04 20:21:21 +0200

    Working rectangle drawing

---
+++ src/Controller.java
@@ -0,0 +1,154 @@
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+import static java.lang.Math.abs;
+
+public class Controller {
+    @FXML
+    private Button okragButton;
+    @FXML
+    private Button prostokatButton;
+    @FXML
+    private Button wielokatButton;
+    @FXML
+    private Button infoButton;
+    @FXML
+    private Button wczytajButton;
+    @FXML
+    private Button zapiszButton;
+    @FXML
+    private Text tooltip;
+    @FXML
+    private Text coords;
+    @FXML
+    private Canvas canvas;
+    @FXML
+    private ColorPicker strokeColorPicker;
+    @FXML
+    private ColorPicker fillColorPicker;
+    @FXML
+    private int activeTool = 1;
+    private boolean isDrawing = false;
+    private boolean primaryClickExists = false;
+    private double primaryPointX;
+    private double primaryPointY;
+
+    @FXML
+    private void selectOkrag(){
+        activeTool = 0;
+        updateTooltip();
+    }
+
+    @FXML
+    private void selectProstokat(){
+        activeTool = 1;
+        updateTooltip();
+    }
+
+    @FXML
+    private void selectWielokat(){
+        activeTool = 2;
+        updateTooltip();
+    }
+
+    @FXML
+    private void showInfo(){
+
+    }
+
+    @FXML
+    private void save(){
+
+    }
+
+    @FXML
+    private void load(){
+
+    }
+
+    @FXML
+    private void updateTooltip(){
+        if(activeTool == 0){
+            tooltip.setText("Okrąg");
+        } else if(activeTool == 1){
+            tooltip.setText("Prostokąt");
+        } else if(activeTool == 2){
+            tooltip.setText("Wielokąt");
+        } else {
+            tooltip.setText("Error");
+        }
+    }
+
+    @FXML
+    private void updateCoords(MouseEvent e){
+        coords.setText("(x,y) = (" + (int) e.getX() + "," + (int) e.getY() + ")");
+        updateTooltip();
+    }
+
+    @FXML
+    private void draw(MouseEvent e) {
+
+        if (isDrawing == false){
+            System.out.println("not drawing yet!");
+            isDrawing = true;
+            GraphicsContext gc = canvas.getGraphicsContext2D();
+            primaryPointX = e.getX();
+            primaryPointY = e.getY();
+        } else {
+            System.out.println("Drawing now");
+            GraphicsContext gc = canvas.getGraphicsContext2D();
+            drawProstokat(gc, e);
+        }
+    }
+    @FXML
+    private void drawOkrag(GraphicsContext gc, MouseEvent e){
+        gc.beginPath();
+        isDrawing = false;
+    }
+
+    @FXML
+    private void drawProstokat(GraphicsContext gc, MouseEvent e){
+        gc.setStroke(strokeColorPicker.getValue());
+        gc.setLineWidth(5);
+        gc.setFill(fillColorPicker.getValue());
+        gc.beginPath();
+        double weed = abs(primaryPointX - e.getX());
+        double height = abs(primaryPointY - e.getY());
+
+        double realPointX = e.getX();
+        double realPointY = e.getY();
+        
+        if(e.getX() > primaryPointX && e.getY() < primaryPointY){
+            realPointX = primaryPointX;
+            realPointY = e.getY();
+        } else if(e.getX() > primaryPointX && e.getY() > primaryPointY){
+            realPointX = primaryPointX;
+            realPointY = primaryPointY;
+        } else if(e.getX() < primaryPointX && e.getY() < primaryPointY){
+            realPointX = e.getX();
+            realPointY = e.getY();
+        } else if(e.getX() < primaryPointX && e.getY() > primaryPointY){
+            realPointX = e.getX();
+            realPointY = primaryPointY;
+        }
+
+
+        gc.rect(realPointX, realPointY, weed, height);
+
+        gc.stroke();
+        gc.fill();
+
+        isDrawing = false;
+    }
+
+    @FXML
+    private void drawWielokat(GraphicsContext gc, MouseEvent e){
+        isDrawing = false;
+
+    }
+}
